9250c52b947263b07015d3372bac3c9f2291b8fa
Jose-Gabriel-Quispe-Mamani <josquispem@unsa.edu.pe>
2025-05-02 19:02:10 -0500

    Calcular crecimiento diario total (suma de confirmados por fecha)

--- TareaAJAX/script2.js
+++ TareaAJAX/script2.js
@@ -4,4 +4,13 @@ fetch('data.json')
     const fechas = data[0].confirmed.map(e => e.date);
     const regionesExcluir = ["Lima", "Callao"];
     const regionesIncluidas = data.filter(e => !regionesExcluir.includes(e.region));
+
+    const crecimiento = fechas.map((fecha, i) => {
+      let total = 0;
+      regionesIncluidas.forEach(region => {
+        const valor = parseInt(region.confirmed[i].value);
+        total += isNaN(valor) ? 0 : valor;
+      });
+      return total;
     });
+  })
