119bad300952d65a8815a674067e25774916602b
Jose-Gabriel-Quispe-Mamani <josquispem@unsa.edu.pe>
2025-05-29 16:49:42 -0500

    Creación del modelo Persona con campos adecuados

--- Django/listaContactos/contactos/models.py
+++ Django/listaContactos/contactos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField(null=True, blank=True)
+    descripcion = models.TextField(blank=True)
+    email = models.CharField(max_length=100, blank=True)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
