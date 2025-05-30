7228f95833f2076fdafc188ff9ecd189a68befd3
Jose-Gabriel-Quispe-Mamani <josquispem@unsa.edu.pe>
2025-05-29 22:35:57 -0500

    Ruta /another conectada a la vista another

--- Django/listaContactos/listaContactos/urls.py
+++ Django/listaContactos/listaContactos/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from inicio.views import home  
+from inicio.views import home, another
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),  
+    path('another/', another),
 ]
