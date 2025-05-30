cd9e4bdd8e98e78ff139cff2d3adc9d819ee9163
Jose-Gabriel-Quispe-Mamani <josquispem@unsa.edu.pe>
2025-05-29 22:34:24 -0500

    Nueva vista 'another' que muestra información del usuario

--- Django/listaContactos/inicio/views.py
+++ Django/listaContactos/inicio/views.py
@@ -2,3 +2,11 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("¡Bienvenido a la página de inicio!")
+
+def another(request):
+    if request.user.is_authenticated:
+        mensaje = f"Hola {request.user.username}, estás autenticado."
+    else:
+        mensaje = "Usuario anónimo: no has iniciado sesión."
+
+    return HttpResponse(mensaje)
