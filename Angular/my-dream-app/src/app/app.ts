c9686d654200dc3b5d12c377465d007c60621975
Jose-Gabriel-Quispe-Mamani <josquispem@unsa.edu.pe>
2025-07-04 17:05:56 -0500

    Agregando Constructor y usando For e Ifs

--- Angular/my-dream-app/src/app/app.ts
+++ Angular/my-dream-app/src/app/app.ts
@@ -1,18 +1,35 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class AppComponent {
   title = 'my-dream-app';
-  name = 'Carlo Corrales D.';
-  email = 'ccorrales@unsa.edu.pe';
-  webpage = 'http://www.unsa.edu.pe';
+  name = "Carlo Corrales D.";
+  email = "ccorrales@unsa.edu.pe";
+  webpage = "http://www.unsa.edu.pe";
+
+  hobbies: string[] = [];
+  filteredHobbies: string[] = [];
+
+  constructor() {
+    this.hobbies = ['Leer', 'Programar', 'Correr',
+       'Videojuegos', 'Cocinar'];
+
+    // Usando for e if para filtrar hobbies con más de 5 letras
+    for (let hobby of this.hobbies) {
+      if (hobby.length > 5) {
+        this.filteredHobbies.push(hobby);
+      }
+    }
+  }
 }
 
 bootstrapApplication(AppComponent);
